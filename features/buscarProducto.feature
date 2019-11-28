17db87794985bb427936eef7279cb9655d065f8b
IceClockPlus <aravenapro98@gmail.com>
2019-11-28 17:20:01 -0300

    Nuevo Escenario para Feature Busqueda Producto
    Se ha agregado un nuevo escenario para el feature Busqueda de Productos

--- features/buscarProducto.feature
+++ features/buscarProducto.feature
@@ -10,3 +10,10 @@ Scenario: Buscar producto por codigo
     When  Ingrese en el campo busqueda "//*[@id='search']" el codigo "29520"
     And   Oprima el boton para buscar "//*[@id='search_btn']"
     Then  Muestra en el listado el producto con el codigo ingresado
+
+
+Scenario: Buscar producto inexistente
+    Given Accede a la url "https://www.pcfactory.cl" para buscar producto inexistente
+    When  Ingresa en el campo busqueda "//*[@id='search']" el producto inexistente "jyfc"
+    And   Oprime el boton para intentar buscar "//*[@id='search_btn']"
+    Then  Muestra el mensaje que indica la inexistencia del producto
--- steps/busquedaProducto.py
+++ steps/busquedaProducto.py
@@ -7,6 +7,8 @@ driver = webapp.get_driver()
 @given(u'Accede a la url "{url}"')
 def acceder_a_la_url(context, url):
     driver.get(url)
+    driver.save_screenshot("C://Evidencia_Prueba//acceder_a_la_url.png")
+    print("Captura Realizada")
 
 
 @when(u'Ingreso en el campo busqueda "{busqueda}" el texto "{valor}"')
@@ -14,6 +16,8 @@ def ingreso_de_nombre_producto_en_el_campo_busqueda(context, busqueda, valor):
     elemen = driver.find_element_by_xpath(busqueda)
     elemen.clear()
     elemen.send_keys(valor)
+    driver.save_screenshot("C://Evidencia_Prueba//ingreso_nombre_producto_en_el_campo_busqueda.png")
+    print("Captura Realizada")
 
 
 @when(u'Oprime el boton para buscar "{boton}"')
@@ -30,17 +34,22 @@ def muestra_lisado_con_productos_coincidentes(context):
     except:
         print("Listado no desplegado")
 
-
+    driver.save_screenshot("C://Evidencia_Prueba//Muestra_el_listado_con_productos.png")
+    print("Captura Realizada")
 
 @given(u'Accede a la pagina "{url}"')
 def acceder_a_la_pagina(context, url):
     driver.get(url)
+    driver.save_screenshot("C://Evidencia_Prueba//accede_a_la_pagina.png")
+    print("Captura Realizada")
 
 @when(u'Ingrese en el campo busqueda "{busqueda}" el codigo "{codigo}"')
 def ingresar_codigo_producto_en_el_campo_busqueda(context,busqueda,codigo):
     elemen = driver.find_element_by_xpath(busqueda)
     elemen.clear()
     elemen.send_keys(codigo)
+    driver.save_screenshot("C://Evidencia_Prueba//ingresar_codigo_producto_en_campo_busqueda.png")
+    print("Captura Realizada")
 
 
 @when(u'Oprima el boton para buscar "{boton}"')
@@ -51,5 +60,38 @@ def oprima_boton_para_buscar(context,boton):
 @then(u'Muestra en el listado el producto con el codigo ingresado')
 def mostrar_listado_producto_con_el_codigo_ingresado(context):
     codProducto = driver.find_element_by_xpath("//*[@id='calugas-productos']/div/div[1]/div[1]/span").text
-    assert codProducto in "29520"
+    try:
+        assert codProducto in "29520"
+    except:
+        print("Producto no encontrado")
+
+    driver.save_screenshot("C://Evidencia_Prueba//mostrar_listado_producto_con_codigo_ingresado.png")
+
+@given(u'Accede a la url "{url}" para buscar producto inexistente')
+def accede_a_url_para_buscar_producto_inexistente(context,url):
+    driver.get(url)
+    driver.save_screenshot("C://Evidencia_Prueba//accede_url_para_buscar_producto_inexistente.png")
+
+
+@when(u'Ingresa en el campo busqueda "{busqueda}" el producto inexistente "{valor}"')
+def ingresa_en_campo_busqueda_producto_inexistente(context,busqueda,valor):
+    elemen = driver.find_element_by_xpath(busqueda)
+    elemen.clear()
+    elemen.send_keys(valor)
+    driver.save_screenshot("C://Evidencia_Prueba//ingresar_en_campo_busqueda_producto_inexistente.png")
+    driver.implicitly_wait(2)
+
+@when(u'Oprime el boton para intentar buscar "{boton}"')
+def oprime_boton_para_intentar_buscar(context,boton):
+    driver.find_element_by_xpath(boton).click()
+
+@then(u'Muestra el mensaje que indica la inexistencia del producto')
+def muestra_mensaje_indica_inexistencia_producto(context):
+    try:
+        mensaje = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div[2]/div[2]/div/div/div/div/h1").text
+        assert mensaje in 'No se encontraron resultados para la búsqueda "jyfc"'
+    except:
+        print("Mensaje no mostrado")
+
+    driver.save_screenshot("C://Evidencia_Prueba//muestra_mensaje_producto_inexistente.png")
     webapp.tearDown()
